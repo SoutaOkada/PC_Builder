6cce57525e0c559c2a742783101f6e7c39ec471c
EC2 Default User <ec2-user@ip-172-31-46-197.ap-northeast-1.compute.internal>
2022-08-13 07:03:33 +0000

    [update] create Cpu model  exe rails g model Cpu edit migration file exe db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_13_062134) do
+ActiveRecord::Schema.define(version: 2022_08_13_065524) do
+
+  create_table "cpus", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.integer "pc_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pc_id"], name: "index_cpus_on_pc_id"
+  end
 
   create_table "pcs", force: :cascade do |t|
     t.integer "cpu_id"
